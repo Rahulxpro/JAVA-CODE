bea50f5f771938c78d21423c66af257c076dffbc
Rahul kumar <117729290+Rahulxpro@users.noreply.github.com>
2022-12-02 17:39:05 +0530

    Switch, if else ladder, if else

---
+++ JavaFirstProgram/src/Com/Company/RKS_EX1_Compare_2_num.java
@@ -0,0 +1,19 @@
+package Com.Company;
+
+import java.util.Scanner;
+
+public class RKS_EX1_Compare_2_num {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Please Enter NUM1 = ");
+        int num1= scan.nextInt();
+        System.out.print("Please Enter NUM2 = ");
+        int num2= scan.nextInt();
+        if(num1>num2){
+            System.out.println("Number1 is Greater then Number2");
+        }else if(num1< num2){
+            System.out.println("Number 1 is Less then Number2");
+        }
+
+    }
+}
---
+++ JavaFirstProgram/src/Com/Company/RKS_EXQ__1.java
@@ -0,0 +1,15 @@
+package Com.Company;
+
+public class RKS_EXQ__1 {
+    public static void main(String[] args) {
+        //Write a java 10!
+        //10!= 10*9*8*7*6*5*4*3*2*1
+        int result = 1;
+        for (int i=10; i>=1; i--){
+
+            result=result*i;
+        }
+        System.out.println(result);
+
+    }
+}
