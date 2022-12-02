c1d29f1f1b54ee6abe8d479823af2198260bb0da
Rahul kumar <117729290+Rahulxpro@users.noreply.github.com>
2022-12-02 17:44:18 +0530

    Switch, if else ladder, if else

--- JavaFirstProgram/src/Com/Company/RKS_EX1_Compare_2_num.java
+++ JavaFirstProgram/src/Com/Company/RKS_EX1_Compare_2_num.java
@@ -10,9 +10,11 @@ public class RKS_EX1_Compare_2_num {
         System.out.print("Please Enter NUM2 = ");
         int num2= scan.nextInt();
         if(num1>num2){
-            System.out.println("Number1 is Greater then Number2");
+            System.out.println("Num1 is Greater then Number2");
         }else if(num1< num2){
-            System.out.println("Number 1 is Less then Number2");
+            System.out.println("Num  is Greater then Number1");
+        }else {
+            System.out.println("num1 is Greater");
         }
 
     }
