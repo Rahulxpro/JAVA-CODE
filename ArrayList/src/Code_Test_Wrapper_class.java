fc8bbacf91eb3257dda0860f94a243776d5c1821
Rahul kumar <117729290+Rahulxpro@users.noreply.github.com>
2022-12-20 10:28:51 +0530

    arraylist Wrapping the class

---
+++ ArrayList/src/Code_Test_Wrapper_class.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Code_Test_Wrapper_class {
+    public static void main(String[] args) {
+        // Creating Arraylist of String
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Wrapper class
+        // A class that wraps (store) primitive data as an object
+
+        Integer y= 18;          //Wrapping the  candy
+        Integer z =y.intValue();// opening the candy
+        list.add(y);
+        list.add(z);
+        System.out.println(list);
+
+    }
+}
